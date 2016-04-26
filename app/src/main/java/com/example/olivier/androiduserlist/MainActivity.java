cfe95c71981da4275029ba835752427a9c96f833
Olivier <olivier.chitthavong@gmail.com>
2016-04-26 19:47:05 +0200

    Modif MainAcitivty

--- app/src/main/java/com/example/olivier/androiduserlist/MainActivity.java
+++ app/src/main/java/com/example/olivier/androiduserlist/MainActivity.java
@@ -46,48 +46,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-  /*      HttpToJson client = new HttpToJson();
+        HttpToJson client = new HttpToJson();
         try {
             client.run();
-            sleep(10);
         } catch (Exception e) {
         }
-        List<UserData> users = client.getList();
+        /*List<UserData> users = client.getList();
         for(UserData user : users){
             System.out.println(user.getName()+" "+user.getPicture());
         }*/
 
-       OkHttpClient client = new OkHttpClient();
-/*	  String run(String url) throws IOException {
-          Request request = new Request.Builder().url(url).build();
-          Response response = client.newCall(request).execute();
-          return response.body().string();
-	 }
-*/
-        public void run() throws Exception {
-            Request request = new Request.Builder()
-                    .url("http://api.randomuser.me/?format=json&results=50")
-                    .build();
 
-            client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-                Gson gson = new GsonBuilder().create();
-                Users results = gson.fromJson(response.body().string(), Users.class);
-                list = results.userList();
-
-                for(UserData user : list){
-                    System.out.println(user.getName()+" "+user.getPicture());
-                }
-                response.body().close();
-            }
-        });
-    }
         //System.out.println(client.getAnswer()+"hey");
         /*
         Gson gson = new GsonBuilder().create();
@@ -104,7 +73,7 @@ public class MainActivity extends Activity {
         //Glide.with(this).load(list.get(0).getPicture()).into(imageView);
 
         TextView text = new TextView(this);
-        text.setText("Bonjour, vous me devez 1 000 000€.");
+        text.setText("Agaprout ");
 
         setContentView(text);
         //setContentView(imageView);
